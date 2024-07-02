edf0b3b1dc4422c51fd21df9479532dc644603fd
Shevon Cooper <657869423@qq.com>
2024-07-02 19:07:52 +0800

    feat:更改数据

--- app/Index/timelinePage.tsx
+++ app/Index/timelinePage.tsx
@@ -5,14 +5,28 @@ import React from 'react'
 import TimelineItem from '../../components/timeLine'
 
 const timelineData = [
-  { date: '2023', title: 'Title 1', description: 'Description 1' },
-  { date: '2022', title: 'Title 2', description: 'Description 2' },
-  { date: '2021', title: 'Title 3', description: 'Description 3' },
-  { date: '2021', title: 'Title 3', description: 'Description 3' },
-  { date: '2021', title: 'Title 3', description: 'Description 3' },
-  { date: '2021', title: 'Title 3', description: 'Description 3' },
-  { date: '2021', title: 'Title 3', description: 'Description 3' },
-  // 添加更多条目
+  //   {
+  //     date: '2019-2024',
+  //     title: 'Beijing Boxin Chuangyi Technology Co., Ltd',
+  //     description: `Led the development of WeChat public accounts, mini-programs, data visualization platforms, tag management systems, and time management systems.
+  // Designed and developed a data visualization intelligent management platform.
+  // Refactored and optimized MVC code to improve efficiency and reduce redundancy across multiple projects.
+  // Developed a component library for dynamic data visualization, improving user experience and data presentation.
+  // Enhanced real-time chart rendering for smoother performance using optimized timer-based mechanisms.`,
+  //   },
+  //   {
+  //     date: '2017-2019',
+  //     title: 'Beijing Xinzhi Lian Technology Co., Ltd',
+  //     description: `Company's Best Productivity Award Led the front-end development for smart city integration systems, including intelligent integration platforms and cultural relics protection systems.
+  // Developed 3D client interaction features using Unity3D and integrated with front-end technologies for seamless user experiences.
+  // Managed the front-end team, ensuring timely delivery of high-quality code and efficient project management.`,
+  //   },
+  {
+    date: '2016-2017',
+    title: 'Tuidi Digital World (Beijing) Technology Culture Co., Ltd',
+    description:
+      'Developed and maintained interfaces and interactive features for VR-related websites Handled daily maintenance and updates for VR information websites, ensuring optimal performance and user engagement.',
+  },
 ]
 
 const TimelinePage: React.FC = () => {
