00f561598d27ee12f57f2951eb70cb87dd55d1f2
Shevon Cooper <657869423@qq.com>
2024-07-02 19:00:51 +0800

    styles:增加svg动画

--- app/Index/Introduction.tsx
+++ app/Index/Introduction.tsx
@@ -4,29 +4,46 @@
 import React from 'react'
 import { Typewriter } from 'react-simple-typewriter'
 import Link from '@/components/Link'
+import AnimatedCartoon from './animate'
 
 const Introduction = () => {
   return (
-    <div className="relative flex min-h-screen flex-col items-center justify-center text-white">
-      <h1 className="mb-4 text-5xl font-bold">
-        Hi There! <span className="animate-wave"> 👋 </span>
-      </h1>
-      <div className="z-10 animate-fadeIn">
-        <h1 className="mb-4 text-5xl font-bold">
-          <Typewriter
-            words={["Hello, I'm Shevon Zhang"]}
-            loop={false}
-            cursor
-            cursorStyle="|"
-            typeSpeed={70}
-          />
+    <div className="relative flex min-h-screen flex-row  items-center justify-center text-white">
+      {/* left */}
+      <div className="basis-3/5">
+        <h1 className="mb-20 text-7xl font-bold">
+          Hi There! <span className="animate-wave"> 👋 </span>
         </h1>
+
+        <div className="z-10 animate-fadeIn">
+          <h1 className="mb-10 text-5xl font-bold">
+            <Typewriter
+              words={["I'm Shevon X.Zhang"]}
+              loop={false}
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+            />
+          </h1>
+        </div>
+        <p className="mb-10 text-4xl">Front-End Developer</p>
+        <p className="text-1xl mb-10">
+          I have over nine years of experience as a front-end developer. I specialize in creating
+          user-friendly interfaces using technologies like Vue.js, JavaScript, and Echarts. I’ve led
+          teams to build smart city systems and data visualization platforms. I love solving
+          problems and making websites and applications that are both beautiful and functional.
+        </p>
+        <div className="z-10 mt-8">
+          <Link
+            href="#about"
+            className="rounded bg-blue-500 px-4 py-2 transition hover:bg-blue-700"
+          >
+            Learn More About Me
+          </Link>
+        </div>
       </div>
-      <p className="text-2xl">Front-End Developer</p>
-      <div className="z-10 mt-8">
-        <Link href="#about" className="rounded bg-blue-500 px-4 py-2 transition hover:bg-blue-700">
-          Learn More About Me
-        </Link>
+      <div className="basis-2/5 translate-x-0 opacity-100">
+        <AnimatedCartoon animationPath="/static/animate/myhomegirl.json" />
       </div>
     </div>
   )
--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function SectionContainer({ children }: Props) {
   return (
-    <section className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</section>
-    // <section className="m-full">{children}</section>
+    // <section className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</section>
+    <section className="container mx-auto px-4">{children}</section>
   )
 }
