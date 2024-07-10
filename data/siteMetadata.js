e74bd5fe51a81cd6c54d9a364e533b5bd771b0f5
Shevon Cooper <657869423@qq.com>
2024-07-10 23:00:48 +0800

    feat:更新github链接

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -12,7 +12,7 @@ const siteMetadata = {
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
+  github: 'https://github.com/shevonne',
   x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
